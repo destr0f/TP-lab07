d9ffd85f5e6c1a6100c880eb3c60473740ded12f
destr0f <testdas@mail.ru>
2026-05-13 20:49:54 +0300

    added custom hunter package

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,5 @@
+hunter_config(
+  timp_message
+  URL "https://github.com/destr0f/TP-lab07-timp-message/archive/refs/tags/v1.0.0.tar.gz"
+  SHA1 624db479dbf64474338f68d55fab14d72796bfab
+)
---
+++ custom_package_demo/main.cpp
@@ -0,0 +1,8 @@
+#include <timp_message/message.hpp>
+
+#include <iostream>
+
+int main()
+{
+  std::cout << timp_message::message() << std::endl;
+}
